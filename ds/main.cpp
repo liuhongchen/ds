731e9ab94d3d4640a36bd6250614bc162205a739
Hansing <halo_0@icloud.com>
2020-09-17 21:09:57 +0800

    init

---
+++ ds/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ds
+//
+//  Created by 刘洪辰 on 2020/9/17.
+//  Copyright © 2020 hansing. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
---
+++ ds/排序/_1_直接插入排序.cpp
@@ -0,0 +1,14 @@
+//
+//  _1_直接插入排序.cpp
+//  ds
+//
+//  Created by 刘洪辰 on 2020/9/17.
+//  Copyright © 2020 hansing. All rights reserved.
+//
+
+#include <stdio.h>
+using namespace std;
+
+int insertSort(int[] arr){
+    
+}
